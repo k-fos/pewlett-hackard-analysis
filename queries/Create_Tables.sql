d6219d5d90a084b66b3787dd33defff01488af33
Kev <k-foster@outlook.com>
2022-02-13 11:53:19 -0600

    deliverable 1 update

--- queries/Create_Tables.sql
+++ queries/Create_Tables.sql
@@ -1,8 +1,10 @@
 --TABLES
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS emp_title;
 DROP TABLE IF EXISTS dept;
 DROP TABLE IF EXISTS emp;
+
 
 CREATE TABLE dept (
 	 dept_no VARCHAR(10) NOT NULL PRIMARY KEY
@@ -37,3 +39,39 @@ CREATE TABLE dept_manager (
 	,FOREIGN KEY (emp_no) REFERENCES emp(emp_no)
 	,FOREIGN KEY (dept_no) REFERENCES dept(dept_no)
 );
+
+CREATE TABLE emp_title (
+	 title_no INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+	,emp_no INT NOT NULL
+	,title VARCHAR(100) NOT NULL
+	,from_date DATE
+	,to_date DATE
+	,FOREIGN KEY (emp_no) REFERENCES emp(emp_no)
+);
+--ALTER SEQUENCE title_no_seq RESTART WITH 10
+
+-- IMPORT DATA
+COPY emp(emp_no, birth_date, first_name, last_name, gender, hire_date)
+FROM 'C:\Users\k-fos\OneDrive\UoM\Data Visualization\Pewlett_Analysis\employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept(dept_no, dept_name)
+FROM 'C:\Users\k-fos\OneDrive\UoM\Data Visualization\Pewlett_Analysis\departments.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_emp(emp_no, dept_no, from_date, to_date)
+FROM 'C:\Users\k-fos\OneDrive\UoM\Data Visualization\Pewlett_Analysis\dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_manager(dept_no, emp_no, from_date, to_date)
+FROM 'C:\Users\k-fos\OneDrive\UoM\Data Visualization\Pewlett_Analysis\dept_manager.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY emp_title(emp_no, title, from_date, to_date)
+FROM 'C:\Users\k-fos\OneDrive\UoM\Data Visualization\Pewlett_Analysis\titles.csv'
+DELIMITER ','
+CSV HEADER;
--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -1,3 +1,16 @@
-SELECT *
-FROM emp
-LIMIT 100
+DROP TABLE IF EXISTS retirement_title;
+
+SELECT e.emp_no
+	,e.first_name
+	,e.last_name
+	,t.title
+	,t.from_date
+	,t.to_date
+INTO retirement_title
+FROM emp e
+  JOIN emp_title t ON e.emp_no = t.emp_no
+WHERE DATE_PART('year', e.birth_date) BETWEEN 1952 AND 1955
+ORDER BY e.emp_no, t.from_date;
+
+SELECT COUNT(*)
+FROM retirement_title;
