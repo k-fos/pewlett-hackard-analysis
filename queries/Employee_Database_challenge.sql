994a4fdb9197caa94ab42e3ba97d2e777bc7bf7d
Kev <k-foster@outlook.com>
2022-02-13 12:21:29 -0600

    finished deliverable 1

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -1,3 +1,7 @@
+--*****************************************
+--   Deliverable 1 Steps 1-7
+--*****************************************
+
 DROP TABLE IF EXISTS retirement_title;
 
 SELECT e.emp_no
@@ -12,5 +16,33 @@ FROM emp e
 WHERE DATE_PART('year', e.birth_date) BETWEEN 1952 AND 1955
 ORDER BY e.emp_no, t.from_date;
 
-SELECT COUNT(*)
-FROM retirement_title;
+--*****************************************
+--   Deliverable 1 Steps 8-15
+--*****************************************
+
+DROP TABLE IF EXISTS retirement_unique_title;
+
+SELECT DISTINCT ON (emp_no) emp_no
+	,first_name
+	,last_name
+	,title
+INTO retirement_unique_title
+FROM retirement_title
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+--*****************************************
+--   Deliverable 1 Steps 16-22
+--*****************************************
+
+DROP TABLE IF EXISTS retiring_titles;
+
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM retirement_unique_title
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+
+
+
